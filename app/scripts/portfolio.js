2b30c83455f6ca7413c4d5da8e2215cf8f559950
Shawn <shawn.naquin@gmail.com>
2015-05-20 02:45:17 -0400

    need to add doubleclick and taphold to bind instead of click

--- app/scripts/portfolio.js
+++ app/scripts/portfolio.js
@@ -1,33 +1,44 @@
+/*global IScroll*/
+'use strict';
 
-function IscrollIt() {
+var myScroll;
+var myScroll2;
 
-	if (parseInt($('#content').css('width')) < 615 ) {
-
-	}
-	else {
-		var myScroll = new IScroll('#dvone', {
-		    mouseWheel: true
-		  });
-
-		var myScroll2 = new IScroll('#dvtwo', {
-		    mouseWheel: true
-		  });
-	}
+function loadIt() {
+	myScroll = new IScroll('#dvone', {mouseWheel: true});
+	myScroll2 = new IScroll('#dvtwo', {mouseWheel: true});
 }
 
-$(window).resize(function() {
-    windowsize = $(window).width();
-    IscrollIt();
+$(document).ready(function() {
+	
+	var whatSize = parseInt($(window).width());
+	
+	$('.wrapper a').bind("click touchstart", function(e) {
+		e.preventDefault();
+		$('img.display').fadeTo(100,0.8);
+		$('img.display').attr('src', $(this).find('img').attr('src')).fadeTo(100,1);
+		$('#detail p').text(function(){
+			$(this).find('img').attr('rel');
+		});
+		$('html,body').animate({scrollTop:0});
+	});
+	
+	if (whatSize > 615) {
+		loadIt();
+	}
+
 });
 
-$(document).ready(function() {
-    IscrollIt();
+$(window).resize(function() {
 
-		$('.wrapper a').click(function(e) {
-	    e.preventDefault();
-	    $('img.display').fadeTo(100,0.8);
-			$('img.display').attr('src', $(this).find('img').attr('src')).fadeTo(100,1);
-			$('#detail p').text($(this).find('img').attr('rel'));
-			$('html,body').animate({scrollTop:0});
-		});
+	var whatSize = parseInt($(window).width());
+
+	  myScroll.destroy();
+    myScroll2.destroy();
+    $(document).removeAttr('style');
+  
+    if (whatSize>615) {
+    	loadIt();
+    }
+
 });
