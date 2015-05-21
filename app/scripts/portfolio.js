d3693d4aacc6aa072f03df3e46e8aba6e8d6c0a1
Shawn <shawn.naquin@gmail.com>
2015-05-21 02:32:47 -0400

    now I just have to add branding: all js working!

--- app/scripts/portfolio.js
+++ app/scripts/portfolio.js
@@ -1,65 +1,82 @@
 /*global IScroll*/
 'use strict';
-
 var myScroll;
 var myScroll2;
 
-function loadIt() {
-	myScroll = new IScroll('#dvone', {mouseWheel: true});
-	myScroll2 = new IScroll('#dvtwo', {mouseWheel: true});
+function loaded () {
+	myScroll2 = new IScroll('#dvtwo', { mouseWheel: true, tap:true });
+	myScroll = new IScroll('#dvone', { mouseWheel: true, tap:true });
 }
+
+/*
+var whatSizeVar = 0;
+
+function whatSize() {
+	whatSizeVar = 0;
+	whatSizeVar += parseInt($(window).width());
+}
+*/
+
+/*
+var ulOneHeight = 0;
+var ulTwoHeight = 0;
+var divHeight = 0;
 
 function widthIt() {
 	
-	var ulOneHeight = 0;
-	var ulTwoHeight = 0;
-
+	ulOneHeight = 0;
+	ulTwoHeight = 0;
+	divHeight = 0;
+	var minus = $('#ultwo li').height();
+	
 	$('#ulone li').each(function() {
 		ulOneHeight += parseInt($(this).outerHeight(true), 10);
 	});
 	
 	$('#ultwo li').each(function() {
+		
 		ulTwoHeight += parseInt($(this).outerHeight(true), 10);
 	});
-
-	var divHeight = Math.max(ulOneHeight, ulTwoHeight);
-
-	$('#ulone, #ultwo').css('height',divHeight);
-
+		whatSize();
+		divHeight = ulOneHeight + ulTwoHeight;
 }
 
+function determine() {
 
+		whatSize();
+		widthIt();
+		
+		if (whatSize>615) {
+			$('#listcolumn').css('height', $(window).height());
+		}
+		else {
+			$('#listcolumn').css('height', divHeight);
+		}
+}
+*/
 $(document).ready(function() {
-	$('#listcolumn, #dvone, #dvtwo').css('height', $(window).height());
 
-	var whatSize = parseInt($(window).width());
-
-		$('.wrapper a').hammer().bind('press click', function(e) {
-			e.preventDefault()
+	loaded();
+	
+	$('.wrapper a').on('tap', function(e) {
+			e.preventDefault();
 		$('img.display').fadeTo(100,0.8);
 		$('img.display').attr('src', $(this).find('img').attr('src')).fadeTo(100,1);
 		$('#detail p').text($(this).find('img').attr('rel'));
 		$('html,body').animate({scrollTop:0});
 	});
-	
-	if (whatSize > 615) {
-		loadIt();
-	}
 
-			widthIt();
+	// determine();
 
 });
+/*
+var res;
+window.onresize=function() {
+    if (res){clearTimeout(res)};
+    res = setTimeout(function(){
+    
+    	console.log("resize triggered");
 
-$(window).resize(function() {
-
-	var whatSize = parseInt($(window).width());
-
-		myScroll.destroy();
-		myScroll2.destroy();
-		$(document).removeAttr('style');
-		
-		if (whatSize>615) {
-			loadIt();
-		}
-
-});
+    },100);
+};
+*/
